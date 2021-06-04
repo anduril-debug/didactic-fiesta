9f343c6938156d8f4d11cdc65728abb0c4ebf0ef
lukaLortkipanidze <luka.lortkifanidze.1@btu.edu.ge>
2021-06-04 22:18:18 +0400

    sorting input visual

--- sort/script.js
+++ sort/script.js
@@ -2,7 +2,7 @@
 main();
 clickBox()
 
-
+// creating main table
 function main(){
 
   var id = 0;
@@ -18,24 +18,77 @@ function main(){
       document.getElementById('container').appendChild(box)
       id++
     }
-
-
   }
-
+  createQuantityRow()
+  return false;
 }
 
-
+// click on individual box ---not finished---
 function clickBox(){
-  console.log("HERE")
   boxes = document.getElementsByClassName('single-box')
   for (var i = 1; i <= 768; i++) {
     var box = boxes[i]
     box.onclick = function(e){
       console.log(e.target)
-      var boxColor = e.target.style.backgroundColor
+      const x = e.target.getAttribute("X")
+      const y = e.target.getAttribute("Y")
+
+      let entireCol = document.querySelectorAll(`[x = "${x}"]`)
+
+
+      //quantity box color
+      entireCol[entireCol.length - 1].style.backgroundColor = "wheat"
+
+      for (let i = 0; i <= y; i++){
+        entireCol[i].classList.remove("clicked")
+      }
+
+      for (let i = y-1; i < 24; i++){
+        entireCol[i].classList.add("clicked")
+      }
+    
+    
+
+          
+      const redBoxCount = 25 - parseInt(y)
+      updateQuantity(redBoxCount,x)
 
     }
   }
 
 
+
+
+  return false;
+}
+
+// creating quantity rows
+function createQuantityRow(){
+
+  for (var i = 1; i <= 32; i++){
+    var quantity = document.createElement("SPAN")
+    quantity.setAttribute("class","col-quantity")
+    quantity.setAttribute("style",`grid-row: 25; grid-column: ${i};`)
+    quantity.setAttribute("col-total", "0")
+    quantity.setAttribute("X", `${i}`)
+    quantity.innerHTML = quantity.getAttribute("col-total")
+    document.getElementById('container').appendChild(quantity)
+  
+
   }
+  
+  return false;
+}
+
+
+//Updating quantity row counts
+function updateQuantity(redBoxCount,colNumber){
+  console.log(`${redBoxCount} witeli ujra, ${colNumber} columnze`)
+  
+  let quantityBox = document.querySelectorAll(`[x = "${colNumber}"]`)
+  quantityBox = quantityBox[quantityBox.length - 1]
+
+  quantityBox.innerHTML = redBoxCount;
+
+  return false 
+}
